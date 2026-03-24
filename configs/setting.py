e7b0e11ccd0df9670f2a3ce4cf6f33b528ffcd02
lililll-study <342032684@qq.com>
2026-03-24 23:37:07 +0800

    Remove sensitive configuration details from setting.py

--- configs/setting.py
+++ configs/setting.py
@@ -1,27 +1,4 @@
 # 聊天模型配置
-chat_model_name = "Qwen/Qwen2.5-72B-Instruct"
-api_key = "sk-zpmvoxslnynnahqfwnqovzwdyrkpfakpijwywdlxonfiybgo"
-base_url = "https://api.siliconflow.cn/v1"
 
-# 临时文件存储路径（用于上传的文件）
-TEMP_FILE_STORAGE_DIR = "./temp/data"
-
-# 媒体文件保存路径
-MEDIA_DIR = "./temp/medias"
-
-# 设置超时
-TIME_OUT = 60
-
-
-
-embedding_model_path = "./models/AI-ModelScope/bge-large-zh-v1___5"
-# 存储数据库的路径
-KB_DIR = "./knowledgebases"
-# 存储文件的路径
-FILE_STORAGE_DIR = "./data"
-# 知识库信息数据库的路径
-SQLALCHEMY_DATABASE = "./database_server/data/"
-# 知识库信息数据库的URI
-SQLALCHEMY_DATABASE_URI = "sqlite:///./database_server/data/info.db"
 
 
